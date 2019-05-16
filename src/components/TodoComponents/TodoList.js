1d71834b14c33255179abe0554dcebc77086d7b9
Cedricium <amaya.cedric@gmail.com>
2019-05-15 18:10:55 -0700

    Finish TodoList.js which maps over todos and creates a new Todo

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react'
+import Todo from './Todo'
+
+const TodoList = (props) => {
+  const { todos, handleToggleCompleted } = props
+  return (
+    <div className="todos-wrapper">
+      {
+        todos.map(todo => <Todo key={todo.id} todo={todo} handleToggleCompleted={handleToggleCompleted} />)
+      }
+    </div>
+  )
+}
+
+export default TodoList
