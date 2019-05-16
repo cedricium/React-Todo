6051f17b0b436e3677e8ce662422e1faf25e58b5
Cedricium <amaya.cedric@gmail.com>
2019-05-15 18:09:10 -0700

    Build out App.js with all data and handler functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,86 @@
 import React from 'react';
 
+import TodoList from './components/TodoComponents/TodoList'
+import TodoForm from './components/TodoComponents/TodoForm'
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super()
+    this.state = {
+      task: '',
+      todos: [
+        {
+          task: 'build a todo app',
+          id: Date.now(),
+          completed: false,
+        }
+      ]
+    }
+
+    this.addTodoHandler = this.addTodoHandler.bind(this)
+    this.clearTodosHandler = this.clearTodosHandler.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleToggleCompleted = this.handleToggleCompleted.bind(this)
+  }
+
+  addTodoHandler(event) {
+    event.preventDefault()
+    const { task, todos } = this.state
+    if (task.length > 0) {
+      const todosCopy = todos.slice()
+      todosCopy.push({
+        id: Date.now(),
+        completed: false,
+        task,
+      })
+      this.setState({
+        todos: todosCopy,
+        task: '',
+      })
+    }
+  }
+
+  clearTodosHandler() {
+    const pendingTodos = this.state.todos.slice().filter(todo => todo.completed === false)
+    this.setState({
+      todos: pendingTodos
+    })
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleToggleCompleted(event) {
+    const todoId = event.target.dataset.todo
+    const index = this.state.todos.findIndex(todo => {
+      return todo.id === Number.parseInt(todoId, 10)
+    })
+    const updatedTodos = [...this.state.todos]
+    updatedTodos[index] = {
+      task: updatedTodos[index].task,
+      id: updatedTodos[index].id,
+      completed: !updatedTodos[index].completed
+    }
+    this.setState({ todos: updatedTodos })
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Todo List: MVP</h1>
+        <TodoForm
+          task={this.state.task}
+          addTodoHandler={this.addTodoHandler}
+          clearTodosHandler={this.clearTodosHandler}
+          handleInputChange={this.handleInputChange}
+        />
+        <TodoList
+          todos={this.state.todos}
+          handleToggleCompleted={this.handleToggleCompleted}
+        />
       </div>
     );
   }
